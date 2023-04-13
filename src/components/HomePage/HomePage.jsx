e48ddeecc88fd5e61832a9743837bcc25800d5f3
patrickhoey20 <patrickhoey2024@u.northwestern.edu>
2023-04-13 12:29:24 -0500

    gpt api working

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -3,9 +3,10 @@ import './HomePage.css'
 import { useDbData } from "../../utilities/firebase.js"
 import { LeftOffPage } from '../LeftOffPage/LeftOffPage'
 import { UpdatesPage } from '../UpdatesPage/UpdatesPage'
-
+import { ChatGPTCall } from "../../utilities/api"
 
 export default function HomePage() {
+    // FIREBASE STUFF
     const [data, error] = useDbData('/');
     const [realdata, setRealData] = useState([])
     useEffect(() => {
@@ -13,6 +14,24 @@ export default function HomePage() {
             setRealData(data)
         }
     }, [data])
+    // CHAT GPT API STUFF
+    const [chatGPTResponse, setChatGPTResponse] = useState(null)
+    async function GPTResponse(prompt) {
+        const response = await ChatGPTCall(prompt)
+        setChatGPTResponse(response.choices[0].text.replace(/\n/g, ''))
+    }
+    let run = false
+    useEffect(() => { // useEffect so it only runs on page load (one API call per page load)
+        if (! run) {
+            run = true
+            GPTResponse('What is the meaning of life?')
+        }
+    }, [])
+    useEffect(() => {
+        if (chatGPTResponse) {
+            console.log(chatGPTResponse)
+        }
+    }, [chatGPTResponse])
     return (<>
                 <LeftOffPage/>
                 <UpdatesPage recent={true}/>
---
+++ src/utilities/api.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react';
+
+export async function ChatGPTCall(prompt) {
+    const response = await fetch('https://api.openai.com/v1/engines/text-davinci-003/completions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${import.meta.env.VITE_GPT_API_KEY}`,
+      },
+      body: JSON.stringify({
+        prompt: prompt,
+        max_tokens: 100,
+      }),
+    })
+    const data = await response.json();
+    return data;
+}  
